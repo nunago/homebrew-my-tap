38085e3ecb9a6cc69b2d0e9649e5249740b2232b
Nuno Agostinho <nuno.agostinho@ctw.bmwgroup.com>
2026-09-01 12:13:11 +0100

    Add Double Commander cask

--- Casks/nunago-double-commander.rb
+++ Casks/nunago-double-commander.rb
@@ -1,21 +1,18 @@
-cask "double-commander" do
-  arch arm: "aarch64", intel: "x86_64"
+cask "nunago-double-commander" do
+  arch arm: "aarch64"
 
   version "1.2.8"
-  sha256 arm:   "7432cf00b9d111730b26ca69d546d40408ca1b8e7ac31253fbc68780799120f0",
-         intel: "2cb6d149fa9b4c993ee6b9323224c26b90181f1bf9c2277049678ee08be3c88d"
+  sha256 arm:   "7432cf00b9d111730b26ca69d546d40408ca1b8e7ac31253fbc68780799120f0"
 
   url "https://github.com/doublecmd/doublecmd/releases/download/v#{version}/doublecmd-#{version}.cocoa.#{arch}.dmg"
   name "Double Commander"
-  desc "File manager with two panels"
+  desc "File manager with two panels side by side. It is inspired by Total Commander and features new ideas."
   homepage "https://doublecmd.sourceforge.io/"
 
   livecheck do
     url :url
     strategy :github_latest
   end
-
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   depends_on macos: :big_sur
 
